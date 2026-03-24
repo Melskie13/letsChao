73bb1b02943d04692510ce14f1ec31f2ea517184
TitoMelssss <globioromel@gmail.com>
2026-03-24 13:12:52 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,35 @@
 const menuList = [
     {
-    name: "Pork Chao Fan Egg",
-    price: 54,
-    category: "friedrice",
-    image: "images/cf-egg.jpg",
-    bestSeller: false,
-    varieties: [
-        { name: "Regular", price: 54 },
-        { name: "Bagoong", price: 64 }
-    ],
-    flavors: ["Spicy", "Honey Garlic", "Parmesan"]
+        name: "Pork Chao Fan Egg",
+        price: 54,
+        category: "friedrice",
+        image: "images/cf-egg.jpg",
+        bestSeller: false,
+        varieties: [
+            { name: "Bagoong Chao Fan", price: 64 }
+        ],
+        flavors: [
+            { name: "Spicy", price: 5 },
+            { name: "Honey Garlic", price: 10 },
+            { name: "Parmesan", price: 15 }
+        ]
     },
-    
+    {
+        name: "Pork Chao Fan Chicken Wings",
+        price: 105,
+        category: "friedrice",
+        image: "images/cf-chickenwings.jpg",
+        bestSeller: true,
+        varieties: [
+            { name: "Bagoong Chao Fan", price:115 }
+        ],
+        flavors: [
+            { name: "Spicy", price: 5 },
+            { name: "Honey Garlic", price: 10 },
+            { name: "Parmesan", price: 15 }
+        ]
+    },
+   
 ];
 
 const addOns = [
