6c091358ade22c46e7ed301c4fca3ab482a658c6
TitoMelssss <globioromel@gmail.com>
2026-03-24 09:23:31 +0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,23 @@
+const menuList = [
+    {
+        name: "Chicken Chao Fan",
+        price: 120,
+        category: "friedrice",
+        image: "images/fried-rice.jpg",
+        bestSeller: true
+    },
+    {
+        name: "Pork Chao Fan",
+        price: 130,
+        category: "friedrice",
+        image: "images/fried-rice.jpg",
+        bestSeller: false
+    },
+    {
+        name: "Iced Coffee",
+        price: 90,
+        category: "coffee",
+        image: "images/coffee.jpg",
+        bestSeller: true
+    }
+];
