587221b98defd2671d4e70f6fdea8a905e75f989
TitoMelssss <globioromel@gmail.com>
2026-03-24 12:42:42 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,20 +4,33 @@ const menuList = [
         price: 120,
         category: "friedrice",
         image: "images/fried-rice.jpg",
-        bestSeller: true
+        bestSeller: true,
+        flavors:""
     },
     {
         name: "Pork Chao Fan",
         price: 130,
         category: "friedrice",
         image: "images/fried-rice.jpg",
-        bestSeller: false
+        bestSeller: false,
+         flavors:""
     },
     {
         name: "Iced Coffee",
         price: 90,
         category: "coffee",
         image: "images/coffee.jpg",
-        bestSeller: true
+        bestSeller: true,
+        flavors:""
     }
+];
+
+const addOns = [
+{
+        name: "Chili Oil",
+        price: 10,
+        image: "images/chili-oil.jpg",
+        
+    },
+    
 ];
